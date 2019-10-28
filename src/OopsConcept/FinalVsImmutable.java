015dc8ea7e20d77baf9b5a59ae0ae80ed121dfb4
ghoshankur <56943306+ghoshankur@users.noreply.github.com>
2019-10-28 14:41:35 -0500

    FinalVsImmutable

---
+++ src/OopsConcept/FinalVsImmutable.java
@@ -0,0 +1,28 @@
+package OopsConcept;
+
+/*final means that you can’t change the object’s reference to point to another reference or another object, 
+ but you can still mutate its state (using setter methods e.g).
+ Whereas immutable means that the object’s actual value can’t be changed, 
+ but you can change its reference to another one.*/
+public class FinalVsImmutable {
+
+	public static void main(String[] args) {
+		final StringBuffer fi = new StringBuffer("Hello ");
+		// Even though reference variable fi is final
+		// We can perform any changes
+		fi.append("Ankur");
+		System.out.println(fi);
+		// Here we will get Compile time error
+		// Because reassignment is not possible for final variable
+		fi = new StringBuffer("Hello");
+
+		// immutability
+		String s1 = "Java";
+		String s2 = "Java";
+		s2 = "Ankur";
+		System.out.println(s1);
+		System.out.println(s2);
+
+	}
+
+}
--- src/OopsConcept/finalConcept.java
+++ src/OopsConcept/finalConcept.java
@@ -3,7 +3,6 @@ package OopsConcept;
 public class finalConcept {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub\
 		 final int i=10;
 		  //final is used for constant value and value can not be changed for that variable.
 		 //Final you can use to prevent inheritance and method overriding also
