0fce671412ae2740cb8102a9dddbe30babf2482a
ghoshankur <56943306+ghoshankur@users.noreply.github.com>
2019-11-04 08:27:40 -0600

    Constructor with this file changes.

--- src/OopsConcept/ConstructorWithThis.java
+++ src/OopsConcept/ConstructorWithThis.java
@@ -4,8 +4,9 @@ public class ConstructorWithThis {
 
 	//Class Variable
 	//This key word is used to initialize the class variable. if you want to use class variable value in any method or constructor you can use this
+	//To initialize class property we use this which is a method or variable. Static will not be part of this.
 	String name;
-	static int age;
+	int age;
 	
 	public ConstructorWithThis(String i,int a){
 		this.name=i;//if you are passing different local variable name to initialize global variable you need not to use this keyword. Here it's used just to differentiate the this key word is used only if global and local variable name are same and you want to initialize the global variable with the local variable 
