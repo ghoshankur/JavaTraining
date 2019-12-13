941c35fe69f50e961a3b12860cd2ec0899e60fc4
ghoshankur <56943306+ghoshankur@users.noreply.github.com>
2019-12-13 12:45:38 -0600

    NameSwap modified

--- src/BasicJavaProgram/SwapWordsInName.java
+++ src/BasicJavaProgram/SwapWordsInName.java
@@ -1,28 +1,32 @@
 package BasicJavaProgram;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class SwapWordsInName {
 
-	public static void SwapName(String s)
-	{
-		String b = null;
-		String w = null;
-		String word[]=s.split(" ");
-		//Map<String,Integer> Count= new HashMap<String,Integer>();
-		for(int i=0;i<word.length;i++)
-		{
-		 	if(i==0)
-		 		w=word[i];
-		 	else if(i>0)
-		 		b=word[i];		
+	public static void SwapName(String s) {
+		String f = null;
+		String l = null;
+		String word[] = s.split(" ");
+		if (word.length < 3) {
+			for (int i = 0; i < word.length; i++) {
+				if (i == 0)
+					f = word[i];
+				else if (i > 0)
+					l = word[i];
+			}
+			System.out.println(l + " " + f);
 		}
-		System.out.println(b+" "+w);
 	}
+	// public static void SwapName(String n)
+	// {
+	// System.out.println("Before Swap:\n"+n);
+	// String firstName=n.substring(0,n.indexOf(" "));
+	// String lastName=n.substring(n.indexOf(" "));
+	// System.out.println("After Swap:\n"+lastName+" "+firstName);
+	// }
+
 	public static void main(String[] args) {
 		SwapName("Ankur Ghosh");
 
 	}
-
 }
